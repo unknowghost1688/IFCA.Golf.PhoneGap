6dd07fb37f62788384939259521a7318ecdadf28
unknowghost1688 <unknowghost1688@gmail.com>
2014-06-10 10:12:21 +0800

    d
    d

--- js/main.js
+++ js/main.js
@@ -59,14 +59,14 @@ if (localStorage.getItem("Token") != "") {
         error: function (jqXHR, exception) {
             if (jqXHR.status === 0) {
                 alert('Not connect.\n Verify Network.');
+            } else if (exception === 'timeout') {
+                alert('Not connect.\n Verify Network.');
             } else if (jqXHR.status == 404) {
                 alert('Requested page not found. [404]');
             } else if (jqXHR.status == 500) {
                 alert('Internal Server Error [500].');
             } else if (exception === 'parsererror') {
                 alert('Requested JSON parse failed.');
-            } else if (exception === 'timeout') {
-                alert('Time out error.');
             } else if (exception === 'abort') {
                 alert('Ajax request aborted.');
             } else {
